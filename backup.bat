39491c655625abf5338e5875dec24d4385974713
RAFAEL SILVA AKUTSU <rafael.sakutsu@senacsp.edu.br>
2024-10-21 16:22:05 -0300

    v.1.0

---
+++ backup.bat
@@ -0,0 +1,4 @@
+mkdir backup
+copy /windows/*.txt /users/rafael.sakutsu/documents/script-backup/backup
+copy /windows/system32/*.log /users/rafael.sakutsu/documents/script-backup/backup
+copy /windows/system32/*.txt /users/rafael.sakutsu/documents/script-backup/backup
